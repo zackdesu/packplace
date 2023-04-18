96e82ac574fa0ba6b90ada841017f0dcf10468bb
zackdesu <zarchxxx@gmail.com>
2023-04-18 13:38:37 +0700

    responsive navbar, logout, sign up & log in

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-// All Imports
 // Express modules
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts');
-const { check, validationResult } = require('express-validator');
+const session = require('express-session')
 
-// BcryptJS
-const bcrypt = require('bcryptjs')
-
-// Method Modules (put & delete)
-const methodOverride = require('method-override');
-
-// Database & Contact
-require('./utilities/db')
-const Contact = require('./utilities/model')
+// Local modules
+const authRoutes = require('./route/auth');
 
 // Calling Import
 const app = express();
@@ -25,140 +17,53 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 
-// Method Override
-app.use(methodOverride('_method'));
+// Session
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 1000 * 60 * 5}
+}))
 
+// login & signup
+app.use(authRoutes);
 
 // Homepage
 app.get('/', (req, res) => {
     try {
         res.render('index', {
             layout: 'layout',
-            title: 'PackPlace'
+            title: 'PackPlace',
+            link: req.path
         })
     } catch (error) {
         console.log(error)
     }
 })
 
+// about
 app.get('/about', (req, res) => {
     try {
         res.render('about', {
             layout: 'layout',
             title: 'PackPlace',
-
+            user: req.session.user,
+            link: req.path
         })
     } catch (error) {
         console.log(error)
     }
 })
-
-// Login
-app.get('/login', (req, res) => {
-    try {
-        res.render('login', {
-            layout: 'layout',
-            title: 'PackPlace',
-        })
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-// Proses login
-app.post('/login', [
-    check('email', 'Email tidak valid!')
-        .isEmail()
-        .custom(async value => {
-            const email = await Contact.findOne({email: value})
-            if(email == null) {
-                throw 'Akun belum terdaftar.'
-            }
-            return true
-        }),
-    check('password', 'Password tidak boleh kosong')
-        .notEmpty()
-], async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        res.render('login', {
-            layout: 'layout',
-            title: 'PackPlace',
-            errors: errors.array(),
-            })
-        }
-    try {
-        const user = await Contact.findOne({email: req.body.email});
-        const isMatch = await bcrypt.compare(req.body.password, user.password);
-        if(!isMatch){
-            console.log( 'Password salah!' )
-        } else{
-            res.redirect('/about')
-        }
-
-    } catch (err) {
-        res.render('login', {
-            layout: 'layout',
-            title: 'PackPlace',
-            errors: errors.array()
-            })
-    }
-})
-
-// Sign Up Page
-app.get('/signup', (req, res) => {
-    try {
-        res.render('signup', {
-            layout: 'layout',
-            title: 'PackPlace'
-        })
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-// Proses sign up
-app.post('/signup', [
-    check('email', 'Email tidak valid!')
-        .isEmail()
-        .custom(async value =>{
-        const duplicate = await Contact.findOne({email: value})
-        if(duplicate) {
-            throw `Email ${value} sudah digunakan, gunakan email lain.`
-        } 
-        return true;
-    })], async (req, res) => {
-    const errors = validationResult(req);
-    // console.log(errors)
-    if(!errors.isEmpty()){
-            res.render('signup', {
-                layout: 'layout',
-                title: 'PackPlace',
-                errors: errors.array(),
-            })
-    } else{
-        const saltRounds = 8;
-
-        const password = req.body.password
-        const salt = await bcrypt.genSalt(saltRounds);
-        const hashedPassword = await bcrypt.hash(password, salt)
-
-        req.body.password = hashedPassword;
-        Contact.insertMany(req.body, (() => {
-            res.redirect('/')
-        }))
-    }
-})
-
-
 
 
 // Error
 app.use('/', (req, res) => {
-    res.status(404);
-    res.render('404', {
+    res
+        .status(404)
+        .render('404', {
         layout: 'layout',
-        title: 'PackPlace'
+        title: 'PackPlace',
+        link: req.path
     })
 })
 
---
+++ route/auth.js
@@ -0,0 +1,151 @@
+// Express
+const express = require('express')
+const { check, validationResult } = require('express-validator');
+
+// BcryptJS
+const bcrypt = require('bcryptjs')
+
+// Database & Contact
+require('../utilities/db')
+const Contact = require('../utilities/model')
+
+// Calling Import
+const router = express.Router();
+
+
+// Proses login #1 ( Wajib diletakkan di baris sesudah session! )
+router.post('/login', [
+    check('email', 'Email tidak valid!')
+        .isEmail()
+        .custom(async value => {
+            const email = await Contact.findOne({email: value})
+            if(email == null) {
+                throw 'Akun belum terdaftar.'
+            }
+            return true
+        }),
+    check('password', 'Password tidak boleh kosong')
+        .notEmpty()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(errors.isEmpty()){
+        try {
+            const user = await Contact.findOne({email: req.body.email});
+            const isMatch = await bcrypt.compare(req.body.password, user.password);
+            if(!isMatch){
+                throw 'Password yang anda masukkan salah!'
+            } else{
+                req.session.regenerate(err => {
+                    try {
+                        if(err)throw err;
+                        
+                        req.session.user = user;
+    
+                        req.session.save((err) => {
+                            if (err) throw err
+                            res.redirect('/about')
+                        })
+    
+                    } catch (error) {
+                        res.send(error)
+                    }
+    
+                })
+            }
+        } catch (errors) {
+            res.render('login', {
+                layout: 'layout',
+                title: 'PackPlace',
+                user: req.session.user,
+                link: req.path,
+                errors,
+                })
+        }
+    } else{
+        res.render('login', {
+            layout: 'layout',
+            title: 'PackPlace',
+            user: req.session.user,
+            link: req.path,
+            errors: errors.array(),
+        })
+        }
+})
+
+// Login
+router.get('/login', (req, res) => {
+    try {
+        res.render('login', {
+            layout: 'layout',
+            title: 'PackPlace',
+            user: req.session.user,
+            link: req.path,
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+// Sign Up Page
+router.get('/signup', (req, res) => {
+    try {
+        res.render('signup', {
+            layout: 'layout',
+            title: 'PackPlace',
+            user: req.session.user,
+            link: req.path,
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+// Proses sign up
+router.post('/signup', [
+    check('email', 'Email tidak valid!')
+        .isEmail()
+        .custom(async value =>{
+        const duplicate = await Contact.findOne({email: value})
+        if(duplicate) {
+            throw `Email ${value} sudah digunakan, gunakan email lain.`
+        } 
+        return true;
+    })], async (req, res) => {
+    const errors = validationResult(req);
+    // console.log(errors)
+    if(!errors.isEmpty()){
+        res.render('signup', {
+            layout: 'layout',
+                title: 'PackPlace',
+                user: req.session.user,
+                link: req.path,
+                errors: errors.array(),
+            })
+    } else{
+        const saltRounds = 8;
+
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        req.body.password = hashedPassword;
+        Contact.insertMany(req.body, (() => {
+            res.redirect('/')
+        }))
+    }
+})
+
+// Logout
+router.get('/logout', (req, res) => {
+    try {
+        req.session.user = undefined;
+        req.session.save((err) => {
+            if(err) throw err;
+            res.redirect('/')
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = router
